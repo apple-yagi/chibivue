3a44fe77e81804178b16b5f888f049403e1ae1ba
apple-yagi <applesryu@gmail.com>
2024-02-14 08:12:25 +0900

    minimum-component

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,18 +1,64 @@
 import { createApp, h, reactive } from "chibivue";
 
-const app = createApp({
-  setup() {
-    const state = reactive({ count: 0 });
-    const increment = () => {
-      state.count++;
+const MyComponent2 = {
+  props: { someMessage: { type: String } },
+
+  setup(props: any, { emit }: any) {
+    return () =>
+      h("div", {}, [
+        h("p", {}, [`someMessage: ${props.someMessage}`]),
+        h("button", { onClick: () => emit("click:change-message") }, [
+          "change message",
+        ]),
+      ]);
+  },
+};
+
+const MyComponent = {
+  props: { someMessage: { type: String } },
+
+  setup(props: any, { emit }: any) {
+    const state = reactive({ message: "hello" });
+    const changeMessage = () => {
+      state.message += "!";
     };
 
-    return function render() {
-      return h("div", { id: "my-app" }, [
-        h("p", {}, [`count: ${state.count}`]),
-        h("button", { onClick: increment }, ["increment"]),
+    return () =>
+      h("div", {}, [
+        h("p", {}, [`someMessage: ${props.someMessage}`]),
+        h("button", { onClick: () => emit("click:change-message") }, [
+          "change message",
+        ]),
+        h(
+          MyComponent2,
+          {
+            "some-message": state.message,
+            "onClick:change-message": changeMessage,
+          },
+          []
+        ),
       ]);
+  },
+};
+
+const app = createApp({
+  setup() {
+    const state = reactive({ message: "hello" });
+    const changeMessage = () => {
+      state.message += "!";
     };
+
+    return () =>
+      h("div", { id: "my-app" }, [
+        h(
+          MyComponent,
+          {
+            "some-message": state.message,
+            "onClick:change-message": changeMessage,
+          },
+          []
+        ),
+      ]);
   },
 });
 
